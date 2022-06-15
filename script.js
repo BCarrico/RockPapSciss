51485e0deaefeaf7eec349a4ea5a4c26e3a5728d
BCarrico <BCarrico89@gmail.com>
2022-06-15 14:01:57 -0700

    Stable version of RPS with UI

--- script.js
+++ script.js
@@ -3,69 +3,67 @@ let gameArray = ["rock", "paper", "scissors"];
 let computerAnswer = "string";
 let playerScore = 0;
 let computerScore = 0;
+let noWinner = true;
 
-    
-game();
-gameWinner();
+const rockBtn = document.querySelector('.rockButton');
+rockBtn.addEventListener('click', () => {
+    console.log("rock clicked");
+    userPromptInput = "rock";
+    checkNoWinner();
+})  
 
-function getUserInput(){
-    userPromptInput = window.prompt("Do you choose Rock, Paper, or Scissors?");
-    userPromptInput = userPromptInput.toLowerCase();
-    checkInput();
-}
+const paperBtn = document.querySelector('.paperButton');
+paperBtn.addEventListener('click', () => {
+    console.log("paper clicked");
+    userPromptInput = "paper";
+    checkNoWinner();
+})
 
-function inputLowerCase(){
-    userPromptInput = userPromptInput.toLowerCase();
-    console.log("Your choice is " + userPromptInput);
-    checkInput();
-}
-
-function checkInput(){
-if (userPromptInput !== "paper" && userPromptInput !== "rock" && userPromptInput !== "scissors"){
-    console.log(userPromptInput);
-    wrongInput = true;
-    console.log(wrongInput);
-    wrongPrompt();
-} else {
-    wrongInput = false;
-    computerPlay();
-} 
-} 
+const scissorsBtn = document.querySelector('.scissorsButton');
+scissorsBtn.addEventListener('click', () => {
+    console.log("scissors clicked");
+    userPromptInput = "scissors";
+    checkNoWinner();
+})
 
 function computerPlay(){
     randomNumber = Math.floor(Math.random()*gameArray.length);
     computerAnswer = gameArray[randomNumber];
     console.log("Computer choice is " + computerAnswer);
     playRound();
+    gameWinner();
 }
 
-function wrongPrompt(){
-    if (wrongInput === true) {
-    userPromptInput = window.prompt("Not an available choice. Try again. Rock, Paper, or Scissors?");
-    inputLowerCase();
+function checkNoWinner(){
+    console.log(noWinner);
+    if (noWinner != false){
+        computerPlay();
+    } else {
+        console.log('Game Over, try again?');
     }
-} 
+}
+
 
 function playRound(player, computer){
     if (userPromptInput === computerAnswer){
-        console.log("You tied... " + userPromptInput + " is the same as " + computerAnswer)
+        roundResult.textContent = "You tied... " + userPromptInput + " is the same as " + computerAnswer
     } else if (userPromptInput === "rock" && computerAnswer === "scissors") {
-        console.log("You win! Rock beats Scissors");
+        roundResult.textContent = "You win! Rock beats Scissors";
         playerScore++;
     } else if (userPromptInput === "paper" && computerAnswer === "rock"){
-        console.log("You win! Paper beats Rock");
+        roundResult.textContent = "You win! Paper beats Rock";
         playerScore++;
     } else if (userPromptInput === "scissors" && computerAnswer === "paper"){
-        console.log("You win! Scissors beat paper");
+        roundResult.textContent = "You win! Scissors beat paper";
         playerScore++;
     } else if (userPromptInput === "rock" && computerAnswer === "paper"){
-        console.log("You lose! Paper beats rock");
+        roundResult.textContent = "You lose! Paper beats rock";
         computerScore++;
     } else if (userPromptInput === "paper" && computerAnswer === "scissors"){
-        console.log("You lose! Scissors beat rock");
+        roundResult.textContent = "You lose! Scissors beat rock";
         computerScore++;
     } else if (userPromptInput === "scissors" && computerAnswer === "rock"){
-        console.log("You lose! Rock beats scissors");
+        roundResult.textContent = "You lose! Rock beats scissors";
         computerScore++;
     } else {
         console.log("playRound error")
@@ -73,23 +71,35 @@ function playRound(player, computer){
 } 
 
 function gameWinner(int, int){
-    if (playerScore === computerScore){
-        console.log("Wow, you tied after all that! Best of 7?");
-    } else if (playerScore > computerScore){
+    if (playerScore === 5){
         console.log("Nice job! You won best of 5! Score was Player " + playerScore +" v Computer score of " + computerScore); 
+        resultsContainer.textContent = "Nice job! You won best of 5! Score was Player " + playerScore +" vs Computer score of " + computerScore;
+        currentScoreContainer.textContent = "Player Score: " + playerScore + "                  vs                  Computer Score: " + computerScore;
+        noWinner = false;
+        
+    } else if (computerScore === 5){
+        console.log("Uh oh, sorry, but you lost. Score was Player " + playerScore +" v Computer score of " + computerScore); 
+        resultsContainer.textContent = "Uh oh, sorry, but you lost. Score was Player " + playerScore +" v Computer score of " + computerScore
+        currentScoreContainer.textContent = "Player Score: " + playerScore + "                  vs                  Computer Score: " + computerScore;
+        noWinner = false;
+        
     } else {
-        console.log("Uh oh, sorry, but you lost. Score was Player " + playerScore +" v Computer score of " + computerScore);
-    }
-}
-
-function game(){
-    for (let roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++) { 
-        getUserInput();
-        console.log(playerScore, computerScore);
+        currentScoreContainer.textContent = "Player Score: " + playerScore + "                  vs                  Computer Score: " + computerScore;
+        noWinner = true;  
     }
 }
 
 
-
-
-
+/* DOM Text Events */
+const resultsContainer = document.querySelector('.results');
+const currentScoreContainer = document.querySelector('.currentScore');
+const roundResult = document.querySelector('.roundResult');
+/*const restartButton = document.createElement('button');
+restartButton.textContent = "Play Again?";
+restartButton.addEventListener('click', () => {
+    console.log("restartClicked")
+    noWinner = true;
+    playerScore = 0;
+    computerScore = 0;
+    resultsContainer.removeChild(restartButton);
+})*/
